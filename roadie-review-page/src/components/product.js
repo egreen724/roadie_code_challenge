1584580216fe4b7314ec479ceae136441bc527ae
github username <github email address>
2020-02-03 13:20:03 -0500

    builds container components

---
+++ roadie-review-page/src/components/product.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class ProductContainer extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <img>
+        <h2>ROADIE COMMUNICATOR - INCLUDES INSTALLATION SOFTWARE</h2>
+        <p>by Roadie </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <ul>
+          <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </li>
+        </ul>
+      </div>
+
+    )
+  }
+
+}
+
+export default ProductContainer 
