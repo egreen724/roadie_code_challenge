392dab6787d979db64f4525bd35f8189aafa281d
github username <github email address>
2020-02-03 17:37:18 -0500

    Changes to form

--- roadie-review-page/src/components/review_form.js
+++ roadie-review-page/src/components/review_form.js
@@ -1,25 +1,63 @@
 import React from 'react'
 
 const ReviewForm = () => {
+
+  state = {
+    rating: '',
+    name: '',
+    title: '',
+    content: ''
+  }
+
+  handleChange = (event) => {
+   let name = event.target.name;
+   let value = event.target.value;
+   this.setState({[name]: value});
+  }
+
+
   return (
     <div className="form">
       <button id="x-button">X</button>
+
       <h1>ADD REVIEW</h1>
 
-      <label>Rating</label>
-      <input> </input>
+      <form>
 
-      <label>Your name</label>
-      <input> </input>
+        <label>Rating</label>
+        <select value={this.state.rating}>
+          <option value="1">One star</option>
+          <option value="2">Two stars</option>
+          <option value="3">Three stars</option>
+          <option value="4">Four stars</option>
+          <option value="5">Five stars</option>
+        </select>
 
-      <label>Review Title</label>
-      <input> </input>
+        <label>Your name</label>
+        <input
+          type='text'
+          name='name'
+          onChange={this.handleChange}
+        />
 
-      <label>Write your review below</label>
-      <input > </input>
+        <label>Review Title</label>
+        <input
+          type='text'
+          name='title'
+          onChange={this.handleChange}
+        />
 
-      <button className="white">Cancel</button>
-      <submit className="teal">Submit</submit>
+        <label>Write your review below</label>
+        <input
+          type='textarea'
+          name='content'
+          onChange={this.handleChange}
+        />
+
+        <button className="white">Cancel</button>
+        <input type='submit' className="teal">Submit</submit>
+      </form>
+
     </div>
   );
 };
